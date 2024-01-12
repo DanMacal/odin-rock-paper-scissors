928e7786cb9f11c12d11defe1f31c1c07361af6a
DanMacal <daniel.macalindong@gmail.com>
2024-01-12 18:02:59 +0200

    user input

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-const compChoices = ['Rock', 'Paper', 'Scissors'];
+// Computer
+const compChoices = ['rock', 'paper', 'scissors'];
 const randomlyChosenOption = getComputerChoice(compChoices);
 
 function getComputerChoice(compChoices) {
     const randomIndex = Math.floor(Math.random() * compChoices.length);
+
     const randomCompChoice = compChoices[randomIndex];
+
     return randomCompChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection > computerSelection) {
-        console.log('You win!');
-    } else if ((playerSelection = computerSelection)) {
-        console.log('A tie! Go again.');
+// User
+function getUserChoice() {
+    const userChoice = prompt('Enter your choice (rock, paper, or scissors): ');
+
+    userChoice = userChoice.toLowerCase();
+
+    if (
+        userChoice === 'rock' ||
+        userChoice === 'paper' ||
+        userChoice === 'scissors'
+    ) {
+        return userChoice;
     } else {
-        console.log('You lose!');
+        alert('Invalid choice. Please enter rock, paper, or scissors.');
+        return getUserChoice();
     }
 }
 
-playRound(0, getComputerChoice);
+// Game Logic
+function playRound(playerSelection, computerSelection) {}
+
+playRound('paper', getComputerChoice);
